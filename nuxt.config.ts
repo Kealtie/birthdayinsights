fba814c56ee9fa9d1829d6b406a091e7f65fcd5c
Niels Klumper <n.klumper@code14.nl>
2022-10-07 10:23:50 +0200

    Install Pinia module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,15 @@
-// https://v3.nuxtjs.org/api/configuration/nuxt.config
-export default defineNuxtConfig({
+import { defineNuxtConfig } from 'nuxt/config';
 
-})
+/**
+ * Main configuration file for Nuxt.js
+ * {@link https://v3.nuxtjs.org/api/configuration/nuxt.config}
+ */
+export default defineNuxtConfig({
+    modules: [
+        /**
+         * Installs the official Nuxt.js module for Pinia.
+         * {@link https://pinia.vuejs.org/introduction.html }
+         */
+        '@pinia/nuxt',
+    ],
+});
